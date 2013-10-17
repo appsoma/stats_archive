d645fe83a6d9f80c5bc288b09d09af68d72e2151
danwinkler <danthemanwinkler@gmail.com>
2013-10-17 17:39:19 -0500

    added try catch block around appsoma_restart

--- server/plugins/appsoma_restart.py
+++ server/plugins/appsoma_restart.py
@@ -2,17 +2,20 @@ try:
 	from ss_plugin import *
 
 	def appsoma_restart_annotator():
-		with open( "appsoma.restart_info" ) as f:
-			info = json.loads( f.read() )
-		
-		if info.get( "monitor_read" ):
-			return []
-		
-		info["monitor_read"] = True
-		with open( "appsoma.restart_info", "w" ) as f:
-			f.write( json.dumps( info ) )
+		try:
+			with open( "appsoma.restart_info" ) as f:
+				info = json.loads( f.read() )
+			
+			if info.get( "monitor_read" ):
+				return []
+			
+			info["monitor_read"] = True
+			with open( "appsoma.restart_info", "w" ) as f:
+				f.write( json.dumps( info ) )
 
-		return ["RESTART: " + info["reason"]] #Annotations return a list of all messages
+			return ["RESTART: " + info["reason"]] #Annotations return a list of all messages
+		except Exception as e:
+			return []
 
 	annotators['appsoma_restart'] = appsoma_restart_annotator
 except Exception, e:
