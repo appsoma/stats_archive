5c9d1005079d60b0131c4035ce0bd9a105a01fec
danwinkler <danthemanwinkler@gmail.com>
2013-10-17 17:00:05 -0500

    added appsoma_restart plugin

---
+++ server/plugins/appsoma_restart.py
@@ -0,0 +1,19 @@
+try:
+	from ss_plugin import *
+
+	def appsoma_restart_annotator():
+		with open( "appsoma.restart_info" ) as f:
+			info = json.loads( f.read() )
+		
+		if info.get( "monitor_read" ):
+			return []
+		
+		info["monitor_read"] = True
+		with open( "appsoma.restart_info", "w" ) as f:
+			f.write( json.dumps( info ) )
+
+		return ["RESTART: " + info["reason"]] #Annotations return a list of all messages
+
+	annotators['appsoma_restart'] = appsoma_restart_annotator
+except Exception, e:
+	print "Error importing appsoma_restart.py: " + str(e)
