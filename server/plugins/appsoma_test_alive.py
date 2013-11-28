a60166ec03da64c4bd173594fc139b3ebfc35a35
Agustin Chiappe Berrini <jnieve@gmail.com>
2013-11-28 10:49:38 -0600

    Testing if the node it's alive using websockets

--- server/plugins/appsoma_test_alive.py
+++ server/plugins/appsoma_test_alive.py
@@ -1,14 +1,27 @@
 try:
 	from ss_plugin import *
 	import urllib2
+	import base64
+	import json
+	#ACB: I'm importing the module in this way, because it can cause name problems with the websocket.py module used by the appsoma node
+	import imp
+	websocket = imp.load_source("websocket","/usr/local/lib/python2.7/dist-packages/websocket.py")
 
 	def appsoma_test_alive_collector():
 		try:
 			with open( "appsoma.base_port" ) as f:
 				port = int(f.read()) + 1
-			response = urllib2.urlopen('https://localhost:' + str(port) + '/test_alive')
+			
+			#ACB: Using websockets to check if it's alive
+			ws = websocket.create_connection("wss://localhost:" + str(port-1), header=[ "Sec-WebSocket-Protocol: base64" ])
+			if ws.send(base64.b64encode(json.dumps({ 'command':'rpc_alive' }))) <= 0:
+				return 0
+			recv = json.loads(base64.b64decode(ws.recv()))
+			return 1 if (recv["command"] == "rpc_alive" and "error" not in recv) else 0
+			'''response = urllib2.urlopen('https://localhost:' + str(port) + '/test_alive')
 			html = response.read()
-			return 1 if (html == "1") else 0
+			return 1 if (html == "1") else 0'''
+			
 		except Exception as e:
 			return 0
 
